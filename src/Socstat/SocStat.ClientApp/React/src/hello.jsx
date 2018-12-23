7be7d438261a8075644a7e88e396918a23e5c5a9
shirman <shirman@skbkontur.ru>
2018-12-23 10:36:14 +0500

    add projects

---
+++ src/Socstat/SocStat.ClientApp/React/src/hello.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div> Hello motherfucker!</div>
+        );
+    }
+}
